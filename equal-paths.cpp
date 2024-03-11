4e945518b822a193e44e80ca0d9d43795d4dcb17
Tri Tong <tmtong@usc.edu>
2024-03-11 22:13:04 +0000

    finish tree traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -9,10 +9,33 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool checkDepth (Node* root, int currH, int& leafH){
+  // if empty tree or no path, then true;
+  if (root == nullptr){
+    return true;
+  }
+  // if both left and right is null, it's a leaf node
+  if(root->left == nullptr && root->right == nullptr){
+    // set height if the first leaf node encountered;
+    if (leafH == -1){
+      leafH = currH;
+    }
+    // otherwise only true if currH same as first leafH
+    return leafH == currH;
+  }
+
+  // check left and right side of tree
+  return checkDepth(root->left, currH + 1, leafH) &&
+    checkDepth(root->right, currH + 1, leafH);
+}
+
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
+    // intialize leafH
+    int leafH = -1;
+    return checkDepth(root, 0, leafH);
 }
 
